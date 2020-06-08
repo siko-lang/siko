0c7fb368b92feb6a657e43344b0f612176ee9001
Laszlo Benedek <laszlo.benedek@gmail.com>
2020-06-08 11:34:54 +0200

    Sikoc tests

--- tests.sh
+++ tests.sh
@@ -17,3 +17,7 @@ mkdir -p sikoc_test_runs
 ./siko.py sikoc_test_runs/cmp std2 sikoc_tests/success/operators/cmp/main.sk
 ./siko.py sikoc_test_runs/logic std2 sikoc_tests/success/operators/logic/main.sk
 ./siko.py sikoc_test_runs/math std2 sikoc_tests/success/operators/math/main.sk
+./siko.py sikoc_test_runs/pipe std2 sikoc_tests/success/operators/pipe/main.sk
+./siko.py sikoc_test_runs/adt std2 sikoc_tests/success/syntax/adt/main.sk
+./siko.py sikoc_test_runs/caseof std2 sikoc_tests/success/syntax/caseof/main.sk
+./siko.py sikoc_test_runs/deriving std2 sikoc_tests/success/syntax/deriving/main.sk
