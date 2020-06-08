cc1f6ada2cc376ce17a285ce7145a07d6f544f7c
Laszlo Benedek <laszlo.benedek@gmail.com>
2020-06-08 08:58:45 +0200

    Sikoc transpiler

--- tests.sh
+++ tests.sh
@@ -12,4 +12,7 @@ cargo run -- ../siko ../std ../comp ../rust_comp ../tests/success/ ../tests/fail
 
 cd ..
 
-./sikoc.py sikoc_cmp_test std2 sikoc_tests/success/cmp/main.sk
+mkdir -p sikoc_test_runs
+
+./sikoc.py sikoc_test_runs/cmp std2 sikoc_tests/success/operators/cmp/main.sk
+./sikoc.py sikoc_test_runs/logic std2 sikoc_tests/success/operators/logic/main.sk
