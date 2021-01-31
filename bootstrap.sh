c655540003b6946e920dd365e9b182753ee4cfde
Laszlo Benedek <laszlo.benedek@gmail.com>
2021-01-31 16:14:52 +0100

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,13 +10,13 @@ date
 rustc --edition=2018 bootstrap/main.rs -o bootstrap/stage0 -O
 echo "Generating stage1"
 date
-find ./sikoc -name *.sk | xargs ./bootstrap/stage0 std2/* -o bootstrap/stage1
+find ./sikoc -name *.sk | xargs ./bootstrap/stage0 std2/* -v -o bootstrap/stage1
 echo "Compiling stage1"
 date
 rustc --edition=2018 bootstrap/stage1_rc.rs -O -o bootstrap/stage1
 echo "Generating stage2"
 date
-find ./sikoc -name *.sk | xargs bootstrap/stage1 std2/* -o bootstrap/stage2
+find ./sikoc -name *.sk | xargs bootstrap/stage1 std2/* -v -o bootstrap/stage2
 echo "Compiling stage2"
 date
 rustc --edition=2018 bootstrap/stage2_rc.rs -O -o bootstrap/stage2
