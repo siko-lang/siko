336e6915690690e06268325e69efc158826dbfe8
Laszlo Benedek <laszlo.benedek@gmail.com>
2021-04-29 17:45:53 +0200

    stuff

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -4,17 +4,9 @@ set -e
 
 ./build.sh
 
-mkdir -p comp
-
-cd siko_tester
-cargo run -- ../siko ../std ../comp ../tests/success/ ../tests/fail/ ../rt/
-
-cd ..
-
 mkdir -p sikoc_test_runs
 if [ ! -e compiled_sikoc ];
 then
     ./compile_sikoc.sh
 fi
-./run_tests.py
-rm -rf compiled_sikoc
+./tests.sh
