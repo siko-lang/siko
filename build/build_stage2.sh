0239ebb0c448ae902dd5ce192eaf3e91b7e8c653
Laszlo Benedek <laszlo.benedek@gmail.com>
2022-09-10 10:16:39 +0200

    More tests and small updates

--- build/build_stage2.sh
+++ build/build_stage2.sh
@@ -7,8 +7,7 @@ cd `dirname $0`
 mkdir -p stage2
 echo "Generating stage2"
 date
-STD2=`ls ../sikoc/std2/*.sk ../sikoc/std2/Json/*.sk`
-find ../sikoc/src -name "*.sk" | xargs ../stage1 $STD2 -v -o stage2/stage2 | ts '[%Y-%m-%d %H:%M:%S]' > stage2/log.txt
+../stage1 ../sikoc/src ../sikoc/std2 -v -o stage2/stage2 | ts '[%Y-%m-%d %H:%M:%S]' > stage2/log.txt
 echo "Compiling stage2"
 date
 rustc --edition=2018 stage2/stage2.rs -O -o stage2/stage2
