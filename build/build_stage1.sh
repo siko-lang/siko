2195edf1c2ffc2d6b7dbf9482e96bb80b4186739
Laszlo Benedek <laszlo.benedek@gmail.com>
2022-10-08 14:38:38 +0200

    Main cleanup

--- build/build_stage1.sh
+++ build/build_stage1.sh
@@ -7,7 +7,8 @@ cd `dirname $0`
 mkdir -p stage1
 echo "Generating stage1"
 date
-../stage0 ../src ../std -v -o stage1/stage1 | ts '[%Y-%m-%d %H:%M:%S]' > stage1/log.txt
+#../stage0 ../src ../std -v -o stage1/stage1 | ts '[%Y-%m-%d %H:%M:%S]' > stage1/log.txt
+../stage0 ../src ../std -v -o stage1/stage1 | ts '[%Y-%m-%d %H:%M:%S]'
 echo "Compiling stage1"
 date
 rustc --edition=2018 stage1/stage1.rs -O -o stage1/stage1
--- build/build_stage2.sh
+++ build/build_stage2.sh
@@ -7,7 +7,8 @@ cd `dirname $0`
 mkdir -p stage2
 echo "Generating stage2"
 date
-../stage1 ../src ../std -v -o stage2/stage2 | ts '[%Y-%m-%d %H:%M:%S]' > stage2/log.txt
+#../stage1 ../src ../std -v -o stage2/stage2 | ts '[%Y-%m-%d %H:%M:%S]' > stage2/log.txt
+../stage1 ../src ../std -v -o stage2/stage2 | ts '[%Y-%m-%d %H:%M:%S]'
 echo "Compiling stage2"
 date
 rustc --edition=2018 stage2/stage2.rs -O -o stage2/stage2
