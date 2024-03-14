577794761eaa4cdee0b029831dc2e4c4d56e9140
Laszlo Benedek <laszlo.benedek@gmail.com>
2024-03-14 14:25:20 +0100

    report

--- Siko/src/siko/location/Report.rs
+++ Siko/src/siko/location/Report.rs
@@ -40,35 +40,43 @@ impl Report {
         let lines = self.location.fileId.getLines();
         let startLine = self.location.span.start.line;
         let endLine = self.location.span.end.line;
-
+        let mut separatorPrinted = false;
         for (lineNumber, line) in lines.iter().enumerate() {
             let lineNumber = lineNumber as i64;
+            let distance =
+                std::cmp::min((lineNumber - startLine).abs(), (endLine - lineNumber).abs());
             if lineNumber >= startLine && lineNumber <= endLine {
-                let highlighted_line = if lineNumber == startLine && lineNumber == endLine {
-                    let start = self.location.span.start.offset as usize;
-                    let end = self.location.span.end.offset as usize;
-                    let mut modifiedLine = String::new();
-                    modifiedLine.push_str(&line[..start]);
-                    modifiedLine.push_str(&line[start..end].yellow());
-                    modifiedLine.push_str(&line[end..]);
-                    modifiedLine
-                } else if lineNumber == startLine {
-                    let start = self.location.span.start.offset as usize;
-                    let mut modifiedLine = String::new();
-                    modifiedLine.push_str(&line[..start]);
-                    modifiedLine.push_str(&line[start..].yellow());
-                    modifiedLine
-                } else if lineNumber == endLine {
-                    let end = self.location.span.end.offset as usize;
-                    let mut modifiedLine = String::new();
-                    modifiedLine.push_str(&line[..end].yellow());
-                    modifiedLine.push_str(&line[end..]);
-                    modifiedLine
+                if distance < 3 {
+                    let highlighted_line = if lineNumber == startLine && lineNumber == endLine {
+                        let start = self.location.span.start.offset as usize;
+                        let end = self.location.span.end.offset as usize;
+                        let mut modifiedLine = String::new();
+                        modifiedLine.push_str(&line[..start]);
+                        modifiedLine.push_str(&line[start..end].yellow());
+                        modifiedLine.push_str(&line[end..]);
+                        modifiedLine
+                    } else if lineNumber == startLine {
+                        let start = self.location.span.start.offset as usize;
+                        let mut modifiedLine = String::new();
+                        modifiedLine.push_str(&line[..start]);
+                        modifiedLine.push_str(&line[start..].yellow());
+                        modifiedLine
+                    } else if lineNumber == endLine {
+                        let end = self.location.span.end.offset as usize;
+                        let mut modifiedLine = String::new();
+                        modifiedLine.push_str(&line[..end].yellow());
+                        modifiedLine.push_str(&line[end..]);
+                        modifiedLine
+                    } else {
+                        line.yellow()
+                    };
+                    println!(" {} {} {}", "|".red(), lineNumber, highlighted_line);
                 } else {
-                    line.clone()
-                };
-
-                println!(" {}{}", "|".red(), highlighted_line);
+                    if !separatorPrinted {
+                        separatorPrinted = true;
+                        println!(" {}", "...");
+                    }
+                }
             }
         }
     }
