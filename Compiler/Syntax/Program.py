2cfc79c565dc04da083cd17a72fb23affa3baf6a
Laszlo Benedek <laszlo.benedek@gmail.com>
2024-10-15 10:11:06 +0200

    stuff

--- Compiler/Syntax/Program.py
+++
@@ -1,8 +0,0 @@
-import Compiler.Syntax.Base as Base
-
-class Program(Base.SyntaxBase):
-    def __init__(self):
-        self.modules = []
-        self.functions = {}
-        self.classes = {}
-    
