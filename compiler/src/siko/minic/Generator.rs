f6f99798458685674ecefbe955b8f054f5dabab0
Laszlo Benedek <laszlo.benedek@gmail.com>
2025-10-11 17:39:37 +0200

    arg

--- compiler/src/siko/minic/Generator.rs
+++ compiler/src/siko/minic/Generator.rs
@@ -476,8 +476,13 @@ impl MiniCGenerator {
             }
             self.dumpFunction(f, &mut output)?;
         }
-
-        writeln!(output, "int main() {{")?;
+        writeln!(output, "int32_t saved_argc = 0;")?;
+        writeln!(output, "uint8_t** saved_argv = NULL;\n")?;
+        writeln!(output, "int32_t get_saved_argc() {{ return saved_argc; }}")?;
+        writeln!(output, "uint8_t** get_saved_argv() {{ return saved_argv; }}")?;
+        writeln!(output, "int main(int argc, char** argv) {{")?;
+        writeln!(output, "   saved_argc = argc;")?;
+        writeln!(output, "   saved_argv = (uint8_t**)argv;")?;
         writeln!(output, "   Main_main();")?;
         writeln!(output, "   return 0;")?;
         writeln!(output, "}}\n\n")?;
